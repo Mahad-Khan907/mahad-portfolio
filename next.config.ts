706800e8686b92a9d159e2e43dfdd25ba67b1409
Mahad Khan <kmahad902@gmail.com>
2026-08-16 22:02:33 +0500

    Change redirect destination from WhatsApp to Gmail

--- next.config.ts
+++ next.config.ts
@@ -9,11 +9,13 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+
   async redirects() {
     return [
       {
         source: "/chat",
-        destination: "https://wa.me/923126569883", // your WhatsApp link
+        destination:
+          "https://mail.google.com/mail/?view=cm&fs=1&to=khan03126569@gmail.com",
         permanent: false,
       },
     ];
